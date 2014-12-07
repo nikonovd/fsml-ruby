ca222f2690c2a9058eb58e081a93cc6b80f2d79b
Dmitri Nikonov <nikonov.dmitri@gmail.com>
2014-12-07 21:18:25 +0000

    Refactoring of state.rb

--- state.rb
+++ state.rb
@@ -4,20 +4,30 @@ class State
 
   attr_reader :name
 
+  private :initialize_transitions, :sanitize_to_state
+
   def initialize(name, initial)
     @name = name
     @initial = initial
     @transitions = Hash.new
   end
 
-  def add_transition(to_state, input, action)
-    @transitions[input] = Array.new if @transitions[input].nil?
-    to_state = self.name if to_state.nil? || to_state.empty?
-    @transitions[input].push(Transition.new(self.name, to_state, input, action))
-  end
-
   def initial?
     @initial == true
+  end
+
+  def add_transition(to_state, input, action)
+    initialize_transitions(input)
+    @transitions[input].push(Transition.new(self.name, sanitize_to_state(to_state), input, action))
+  end
+
+  def sanitize_to_state(to_state)
+    to_state = self.name if to_state.nil? || to_state.empty?
+    return to_state
+  end
+
+  def initialize_transitions(input)
+    @transitions[input] = Array.new if @transitions[input].nil?
   end
 
 end
